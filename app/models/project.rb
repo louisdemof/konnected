7eff4af621df56c1c44fe377abc3b3e13c60be9b
mailys renson <mailys_94@hotmail.com>
2020-08-24 17:06:53 +0200

    added model solicitation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+  has_many :solicitations
   belongs_to :user
   belongs_to :level
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_131922) do
+ActiveRecord::Schema.define(version: 2020_08_24_143632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 2020_08_24_131922) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["level_id"], name: "index_projects_on_level_id"
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "solicitations", force: :cascade do |t|
+    t.string "status"
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_solicitations_on_project_id"
+    t.index ["user_id"], name: "index_solicitations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
