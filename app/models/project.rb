b2bcb77d4691404a22a7c35313253b00f8779a3d
mailys renson <mailys_94@hotmail.com>
2020-08-24 17:40:06 +0200

    added the project pages model and fixed some bugs with the project features

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   has_many :solicitations
   has_many :project_features
+  has_many :project_pages
   belongs_to :user
   belongs_to :level
   validates :name, presence: true
