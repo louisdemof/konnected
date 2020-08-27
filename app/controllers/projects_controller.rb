8e00f32638376069cb919ebb1c586cc92f937bfa
mailys renson <mailys_94@hotmail.com>
2020-08-27 15:17:24 +0200

    added buttons to the home page of client

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -30,6 +30,12 @@ class ProjectsController < ApplicationController
     redirect_to project_path(@project)
   end
 
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to root_path
+  end
+
   private
 
   def project_params
