a7eb8e36f86bd37009ba96ff5a4c03bb4415091f
Louis de Moffarts <louisdemof@gmail.com>
2020-09-01 16:12:52 +0200

    Notif backend done Action cable added

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -6,7 +6,12 @@ const initNotificationCable = () => {
     const id = notificationsContainer.dataset.userId;
     consumer.subscriptions.create({ channel: "NotificationChannel", id: id }, {
       received(data) {
-        console.log(data)
+        // select the notif bubble
+        const notifCounter = document.querySelector('.badge')
+        // Add one to the number
+        let counter = parseInt(notifCounter.innerText) + 1
+        notifCounter.innerText = `${counter}`
+
         notificationsContainer.insertAdjacentHTML('afterbegin', data) ;
      // notificationsContainer.insertAdjacentHTML('beforeend', data) ; // called when data is broadcast in the cable
       },
