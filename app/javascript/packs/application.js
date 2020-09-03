238e08c0a9c48d6e346d45d99e29f93397b4f6df
mailys renson <mailys_94@hotmail.com>
2020-09-03 10:45:35 +0200

    added confirmation table to confirmation stage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,6 +32,7 @@ import { InitPriceTotal } from './price_total';
 import { initIntermediaryUpdateCable } from '../channels/intermediary_update_channel';
 import { initNotificationCable } from '../channels/notification_channel';
 import { markAsRead} from './notification';
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
@@ -40,6 +41,7 @@ document.addEventListener('turbolinks:load', () => {
   initNotificationCable();
   markAsRead();
 });
+
 import { initStarRating } from '../plugins/init_star_rating';
 
 initStarRating();
