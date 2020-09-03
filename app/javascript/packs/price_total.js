ebba1e055e3f3f211683264f94fc3a9e96890370
Lennert Govaerts <lennert_govaerts@hotmail.com>
2020-09-03 16:47:13 +0200

    javascript fix

--- app/javascript/packs/price_total.js
+++ app/javascript/packs/price_total.js
@@ -1,25 +1,29 @@
 const initPriceTotal = () => {
-  let form_element = document.querySelector(".edit_project")
-  if (form_element) {
-    let features_fieldset = form_element.querySelector('.project_features')
-    let pages_fieldset = form_element.querySelector('.project_pages')
+  console.log("azekjlazkejlkazjeklajzklaelkj")
+    let features_fieldset = document.querySelector('.project_features')
+    let pages_fieldset = document.querySelector('.project_pages')
     let checkboxs = document.querySelectorAll("fieldset .form-check-input")
     let init_total = parseInt(document.getElementById('total').dataset.total, 10)
-
+    console.log(pages_fieldset)
     if(pages_fieldset)
     {
     pages_fieldset.addEventListener("change", (event) => {
+    console.log(event)
     var result = init_total
     checkboxs.forEach(checkbox => {
       if (checkbox.checked === true) {
       const price = document.getElementById(checkbox.value).innerText;
       const amount = parseInt(price);
+      console.log(amount)
+
       result = result + amount / 100;
       }
     })
+    console.log(document.getElementById('total'))
     document.getElementById('total').innerHTML = result;
     });
-    } else if(features_fieldset)
+    }
+    if(features_fieldset)
     {
     features_fieldset.addEventListener("change", (event) => {
     var result = init_total
@@ -32,9 +36,8 @@ const initPriceTotal = () => {
     })
     document.getElementById('total').innerHTML = result;
     });
-  }
+    }
 
-  }
 };
 
 export { initPriceTotal };
